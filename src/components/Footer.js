a4516dc96ead6ebdf955e163d1e37e6e26275783
Eugenio Gigante <giganteeugenio2@gmail.com>
2024-10-01 17:47:52 +0200

    starting skeleton

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+function Footer() {
+	return (
+		<footer style={{display: "flex"}}>
+			<ul>
+				DoormatNavigation
+				<li><a href="">Home</a></li>
+				<li><a href="">About</a></li>
+				<li><a href="">Menu</a></li>
+				<li><a href="">Reservations</a></li>
+				<li><a href="">Order Online</a></li>
+				<li><a href="">Login</a></li>
+			</ul>
+			<ul>
+				Contact
+				<li><a href="">Adres</a></li>
+				<li><a href="">Phone Number</a></li>
+				<li><a href="">Email</a></li>
+			</ul>
+			<ul>
+				Social Media Links
+				<li><a href="">Instagram</a></li>
+				<li><a href="">Facebook</a></li>
+			</ul>
+		</footer>
+	)
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+	return (
+		<header>
+			<img src="icon_assets/Logo.svg" alt="logo"/>
+		</header>
+	)
+}
+
+export default Header;
